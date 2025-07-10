2c28e9c572d3d2824a8ff7c3a1096fe9fd78bdf7
Adarsh Shukla <adarshukla.27@gmail.com>
2025-07-10 21:50:45 +0530

    Make some changes in settings.py

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'   
 
-ALLOWED_HOSTS = ['yourdomain.com', 'www.yourdomain.com', 'localhost']
+ALLOWED_HOSTS = ['https://blogbrew-4a8y.onrender.com/']
 
 
 # Application definition
